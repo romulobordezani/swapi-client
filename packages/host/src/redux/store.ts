b1aa7b32367194dedf853692f7b00b9263917e2f
Rômulo Bordezani <romulobordezani@gmail.com>
2023-10-24 23:20:57 -0300

    feature(redux-persist): Adds initial basic setup for Redux Persist

--- packages/host/src/redux/store.ts
+++ packages/host/src/redux/store.ts
@@ -1,16 +1,35 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { CurriedGetDefaultMiddleware } from '@reduxjs/toolkit/dist/getDefaultMiddleware';
+import localStorage from 'redux-persist/lib/storage';
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist';
 import { swapiV2 } from '../services/swapi';
+import counterReducer from './features/popular/popular.slice';
 import { setupListeners } from '@reduxjs/toolkit/query';
+
+const persistConfig = {
+  key: 'root',
+  storage: localStorage
+};
+
+const persistedPopularReducer = persistReducer(persistConfig, counterReducer);
 
 export const store = configureStore({
   reducer: {
-    [swapiV2.reducerPath]: swapiV2.reducer
+    [swapiV2.reducerPath]: swapiV2.reducer,
+    popular: persistedPopularReducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(swapiV2.middleware)
+  middleware: (getDefaultMiddleware: CurriedGetDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    }).concat(swapiV2.middleware)
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
+
+export const persister = persistStore(store);
 
 setupListeners(store.dispatch);
