d38a4be18c4cd6f5c7bf849835dab0cc87a9973c
Rômulo Bordezani <romulobordezani@gmail.com>
2023-10-24 23:11:58 -0300

    Improves responsive fonts

--- packages/design-system/src/global.style.ts
+++ packages/design-system/src/global.style.ts
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import { largeScreen } from './theme';
 
 const globalStyle = css`
   html,
@@ -8,13 +9,17 @@ const globalStyle = css`
     background: white;
     height: 100%;
     height: 100%;
-    font-family: Helvetica, Arial, sans-serif;
+    font-family: 'lato', sans-serif;
     background: #111111;
     color: #ffffff;
     font-size: 12px;
 
     a {
       color: #ffffff;
+    }
+
+    @media ${largeScreen} {
+      font-size: 16px;
     }
   }
 
